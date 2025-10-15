4d972b391d69d850c60f07b77ac2362d73d2ce5f
Mawell2503 <06.mawell@gmail.com>
2025-10-15 09:36:58 +0000

    m

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,5 +8,9 @@
 int main(void)
 {
 	printf("the size of char: %u bytes\n",sizeof(char));
+	printf("the size of int: %u bytes\n",sizeof(int));
+	printf("the size of long int: %u bytes\n",sizeof(long int));
+	printf("the size of long long int: %u bytes\n",sizeof(long long int));
+	printf("the size of float: %u bytes\n",sizeof(float));
 	return (0);
 }		
