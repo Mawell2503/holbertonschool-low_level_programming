081b126b435d54a6d0ccd2ab52712c2ff7308cc8
Your Name <you@example.com>
2025-10-16 09:30:06 +0000

    m

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - prints Positive anything is better than negative nothin
+* Return: 0
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if(n>0)
+		printf("is positive"\n)
+	else if(n<0)
+		printf("is negative"\n)
+	else
+		printf("is zero"\n)
+	return (0);
+}
