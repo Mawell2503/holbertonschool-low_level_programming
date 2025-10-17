49501a1a4a494d5194953cc735fb04f07ecab5cd
Your Name <you@example.com>
2025-10-17 07:16:43 +0000

    m

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -20,8 +20,7 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, d);
 	else if (d == 0)
 		printf("Last digit of %d is %d and is 0\n", n, d);
-	else 
+	else
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, d);
-	
 	return (0);
 }
