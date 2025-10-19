d6b61645e5a531854e2bfc4fa8ef0119362fabd3
Your Name <you@example.com>
2025-10-19 10:23:03 +0000

    m

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - prints Hello, world
+* Return: 0
+*/
+
+int main(void)
+{
+	int po;
+
+	for(po = '1'; po <= '9'; po++)
+	putchar(po);
+	putchar('\n');
+
+	return (0);
+}
