47f326ef0c61ae75659dc61505d148acb260fcda
Your Name <you@example.com>
2025-10-21 10:30:01 +0000

    m

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - prints Hello, world
+* main - base 16
 * Return: 0
 */
 
@@ -10,11 +10,11 @@ int main(void)
 	char h;
 	
 	
-	{for (h = '0'; h <= '9'; h++)
-		putchar(h);}
+	for (h = '0'; h <= '9'; h++)
+		putchar(h);
 
-	{for (h = 'A'; h>= 'F'; h++)
-		putchar(h);}
+	for (h = 'A'; h>= 'F'; h++)
+		putchar(h);
 	putchar('\n');
 
 	return(0);
