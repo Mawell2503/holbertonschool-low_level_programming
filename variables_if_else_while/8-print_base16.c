901922129dbe34076929505de6ef102007d65f27
Your Name <you@example.com>
2025-10-21 09:49:18 +0000

    m

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -13,8 +13,8 @@ int main(void)
 	{for (h = '0'; h <= '9'; h++)
 		putchar(h);}
 
-		{for (h = 'A'; h>= 'F'; h++)
-			putchar(h);}
+	{for (h = 'A'; h>= 'F'; h++)
+		putchar(h);}
 	putchar('\n');
 
 	return(0);
