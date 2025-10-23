5219dcaf85a359eeebc5ef2eb04d1997db33487d
Your Name <you@example.com>
2025-10-23 07:33:43 +0000

    m

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<main.h>
+
+/**
+* main - prints Hello, world
+* Return: 0
+*/
+
+int main(void)
+{
+        printf ("_putchar\n");
+        printf ('\n');
+
+        return(0);
+}
