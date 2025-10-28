d79bd3719e889ffa82adca1c2238674a9b72b710
Your Name <you@example.com>
2025-10-28 10:27:59 +0000

    m

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* proto - int _islower(int c);
+* @c: bla
+*
+* Return: 1 if c is a digit, 0 otherwise
+*/
+
+int _islower(int c);
+{
+	char c;
+
+	if(c = 'a-z')
+		_putchar(1);
+	else
+		-putchar(0);
+}
