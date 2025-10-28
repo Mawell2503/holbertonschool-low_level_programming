7b2d64cb31f6632b8621a22cbabee483879ca22e
Your Name <you@example.com>
2025-10-28 08:00:22 +0000

    m

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,13 @@
 
 void print_alphabet_x10(void)
 {
-        int line;
-        char c;
+	int line;
+	char c;
 
-        for (line = 0; line < 10; line++)
-        {
-                for (c = 'a'; c <= 'z'; c++)
-                        _putchar(c);
-                _putchar('\n');
-        }
+	for (line = 0; line < 10; line++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
 }
