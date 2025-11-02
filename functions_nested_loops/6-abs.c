eba027225ebb58cda7f997dd593e609270009a6a
Your Name <you@example.com>
2025-11-02 13:53:34 +0000

    m

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* 6-abs.c - find absolute value
+* @c: bla
+*
+* Return: 1 if c is a digit, 0 otherwise
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+	return (n);
+}
