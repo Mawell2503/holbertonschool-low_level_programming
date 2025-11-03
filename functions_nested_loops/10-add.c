486f3ee6efaa3a455040ec70fee30570507d6039
Your Name <you@example.com>
2025-11-03 06:31:14 +0000

    m

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* add - bla bla
+* @c: bla
+*
+* Return: 1 if c is a digit, 0 otherwise
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* times_table - bla bla
+* @c: bla
+*
+* Return: 1 if c is a digit, 0 otherwise
+*/
+
+void times_table(void)
+{
+        int factX;
+        int factY;
+        int prod;
+
+        for (factX = 0;factX <= 9;factX++)
+        {
+                for (factY = 0;factY <= 9;factY++)
+                {
+                        prod = factX * factY;
+
+                        if (factY == 0)
+                        {
+                                _putchar(prod + '0');
+                        }
+                        else
+                        {
+                                _putchar(',');
+                                _putchar(' ');
+
+                                if (prod < 10)
+                                {
+                                        _putchar(' ');
+                                }
+                                else
+                                {
+                                        _putchar((prod / 10) + '0');
+                                _putchar((prod % 10) + '0');
+                                }
+                        }
+                }
+                _putchar('\n');
+        }
+}
