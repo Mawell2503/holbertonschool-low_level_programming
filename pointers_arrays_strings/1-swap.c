62f19b7a82e50d38a715659f08b03234e92772e8
Your Name <you@example.com>
2025-11-04 11:34:21 +0000

    m

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* proto - bla bla
+* @c: bla
+*
+* Return: 1 if c is a digit, 0 otherwise
+*/
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
