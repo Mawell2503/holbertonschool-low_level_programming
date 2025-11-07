5a38c87df9f87732e1fb0d50d6913b09d0e38920
Your Name <you@example.com>
2025-11-07 09:19:47 +0000

    m

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_rev - print in reverse
+ *@s: points to a char
+ *
+ *Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int i =0;
+
+	while (s[1] != '\0')
+		i++;
+
+	for (i = i - 1; i >= 0;i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
+
