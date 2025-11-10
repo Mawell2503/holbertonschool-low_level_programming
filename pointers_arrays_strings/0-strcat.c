a0d773d20446b5762a1816287cf5b8ba3cc46d2e
Your Name <you@example.com>
2025-11-10 19:15:27 +0000

    m

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcat - concatenates two string
+ * @: dest is a pointer to a char
+ * @: src is a second pointer to a char
+ *
+ * Return: 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int
+}
