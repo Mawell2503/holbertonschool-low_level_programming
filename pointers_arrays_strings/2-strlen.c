9b857a3dfb46f955ec29359607fb6e12305b98bc
Your Name <you@example.com>
2025-11-10 12:36:01 +0000

    m

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -10,13 +10,10 @@
 
 int _strlen(char *s)
 {
-	int length = 0;		/*tarta counter so you can keep of how many characters are in your string*/
+	int length = 0;
 
-	for(;*s != '\0';s++)	/*for loop has ( initial value:initialization; condition; increment/decrement*/
-				/* (;) -> initialization not needed  because 's' already starts at the beginning of the string*/
-				/* (*s != '\0') -> condition: as long as what "s points too(*s)" does "not equal(!=)" to the "null terminator('\0')"*/
-				/* (s++) -> increase s(not what s points to(*s))*/
+	for ( ; *s != '\0'; s++)
 		length++;
 
-	return length;
+	return (length);
 }
