ca7aac94f39e681e80899e65e2f6b57049c7a47f
Your Name <you@example.com>
2025-11-10 12:39:26 +0000

    m

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /*
- * _puts - prints 
+ * _puts - prints a string folowed by a new line
  * @str: pointer to a char
  *
  * Return:0
@@ -10,7 +10,7 @@
 
 void _puts(char *str)
 {
-	while(*str != '\0')
+	while (*str != '\0')
 	{
 		_putchar(*str);
 		str++;
