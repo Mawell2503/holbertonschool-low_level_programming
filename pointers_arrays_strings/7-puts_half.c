273e328edf5cb709384bb0c4a1e2948b2c65193b
Your Name <you@example.com>
2025-11-10 12:22:28 +0000

    m

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: pointer to a char
+ *
+ * Return:0
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int nlen;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	nlen = i;
+	i = nlen / 2;
+
+	while (i < nlen)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
+
+
