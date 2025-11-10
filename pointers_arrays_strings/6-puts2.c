de28110be680e10c7afb8504c421ea8a621efd5a
Your Name <you@example.com>
2025-11-10 11:15:18 +0000

    m

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - prints every other character
+ * @str: pointer to a character
+ *
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
