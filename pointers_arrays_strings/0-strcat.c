ce30fe2438fda204c382fc0d4a62289ad11b0947
Your Name <you@example.com>
2025-11-11 08:46:41 +0000

    m

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -3,13 +3,28 @@
 
 /**
  * _strcat - concatenates two string
- * @: dest is a pointer to a char
- * @: src is a second pointer to a char
+ * @dest: is a pointer to a char
+ * @src: is a second pointer to a char
  *
  * Return: 0
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int
+	int rd = 0;
+	int rs = 0;
+
+	while (dest[rd] != '\0')
+	{
+		rd++;
+	}
+
+	while (src[rs] != '\0')
+	{
+		dest[rd] = src[rs];
+		rd++;
+		rs++;
+	}
+	dest[rd] = '\0';
+	return (dest);
 }
