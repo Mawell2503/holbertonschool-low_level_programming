eacbf747fbf1fb81fb29212924104ad2e889054c
Your Name <you@example.com>
2025-11-14 06:25:10 +0000

    m

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the number of an argument passed into it
+ * @argc: count of arguments
+ * @argv: aray of string
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d/n",argc - 1);
+	return 0;
+}
