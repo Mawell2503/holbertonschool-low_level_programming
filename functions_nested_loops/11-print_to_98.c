8e28c76396577108f2c13eaf4242823c3f04e590
Your Name <you@example.com>
2025-11-17 17:04:49 +0000

    m

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-
+ 
 /**
 * print_to_98 - prints all natural number till 98
 * @n:variable of an integer
@@ -8,56 +8,48 @@
 * Return: 1 if c is a digit, 0 otherwise
 */
 
-void print_to_98(int n)
+void printw(int n)
 {
-	if (n < 98)
+	if (n < 0)
 	{
-		for (; n != 98; n++)
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+		printw(n / 10);
+
+	_putchar((n % 10 ) + '0');
+}
+
+void print_to_98(int n)
+{ 
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
 		{
-			if ( n < 10)
-			{
-				_putchar(n + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
+			printw(n);
 
-			else if (n == 98)
+			if (n != 98)
 			{
-				_putchar(n + '0');
-			}
-
-			else
-			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
 				_putchar(',');
 				_putchar(' ');
 			}
 		}
 	}
 
-	if (n > 98)
+	else
 	{
-		for (; n != 98; n--)
+		for (; n >= 98; n--)
 		{
-			if (n >= 100)
+			printw(n);
+
+			if (n != 98)
 			{
-				_putchar((n / 100) + '0');
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
 				_putchar(',');
 				_putchar(' ');
 			}
-
-			else if (n == 98)
-			{
-				_putchar(n + '0');
-			}
-
-			else
-			{
-				_putchar((n / 10) + '0');
-			}
 		}
 	}
+	_putchar('\n');
 }
