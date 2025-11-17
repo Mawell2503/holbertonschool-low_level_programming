a36ba72a16530c58447aee933de9eaa196b11416
Your Name <you@example.com>
2025-11-17 22:52:00 +0000

    m

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,3 +10,27 @@
 
 int _atoi(char *s);
 {
+	int i = 0;
+	int sign = 1;
+	int num = 0;
+	int found_digit = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign = sign * -1
+		else if (s[i] == '+')
+			;
+		else if(s[i] >= '0' && s[i] <= '9')
+		{
+			found_digit = 1;
+			num = num * 10 + (s[i] - '0');
+		}
+		else if (found_digit)
+			break;
+
+		i++
+	}
+
+	return (num * sign);
+}
