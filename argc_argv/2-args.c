c8ea2a4fd6dcb8cf19278f34d4b04e9626501236
Your Name <you@example.com>
2025-11-18 10:08:27 +0000

    m

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include "main.h"
 
-int main(int argc, char* argv[])
+/**
+ * main - prints all argument it will receive
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
 {
-	printf
+	int i = 0;
+
+	for (; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
