16a8eab3ddaf09ac6269bf812ffa289dbec148a4
Your Name <you@example.com>
2025-11-25 07:35:38 +0000

    m

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -11,5 +11,22 @@
 
 char *_strdup(char *str);
 {
+	char *dup;
+	unsigned int i, len = 0;
 
+	if(str == NULL)
+		return NULL;
+
+	while (str[len] != '\0')
+		len++;
+
+	dup = malloc((len + 1) * sizeof(char));
+	if (dup == NULL)
+		return NULL;
+	for (i = 0; i < len; i++)
+		dup[i] = str[i];
+
+	dup[len] = '\0';
+
+	return dup;
 }
--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,5 +12,30 @@
 
 char *str_concat(char *s1,char *s2)
 {
+	unsigned int i, j, len1 = 0, len2 = 0;
+	char *newstr;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
+
+	newstr = malloc((len1 + len2 + 1) * sizeof(char));
+	if (newstr == NULL)
+		return NULL;
+
+	for (i = 0; i < len1; i++)
+		newstr[i] = s1[i];
+
+	for (j = 0; j < len2; j++)
+		newstr[i + j] = s2[j];
+
+	newstr[len1 + len2] = '/0';
+
+	return newstr;
 }
