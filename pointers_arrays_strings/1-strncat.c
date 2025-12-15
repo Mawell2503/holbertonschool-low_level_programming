f5566ab6b7cbb04e0ab14906c657dcd4c72b810e
Your Name <you@example.com>
2025-12-15 10:29:28 +0000

    m

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * _strncpy - merges 2 strings together
+ * _strncat - merges 2 strings together
  * @dest: pointer of the first string
  * @src:pointer of the second string
  * @n:a vriable to an integer
@@ -14,17 +14,17 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int d = 0;
 	int s = 0;
-	int nd;
 
 	if (dest[d] != '\0')
 		d++;
 
-	
-	if (src[s] != '\0')
-		dest[d] = src[s];
-		d++;
+	if (src[s] < n)
 		s++;
 
-	dest[d]  '\0';
+	dest[d] = src[s];
+	d++;
+	s++;
+
+	dest[d] = '\0';
 	return (dest);
 }
---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reverse_array - reversing the content of an array of an integers
+ * @a: pointer of the array of an integer
+ * @n: variable to an integer
+ *
+ * Return: 1
+ */
+
+ void reverse_array(int *a, int n)
+{
+
+
+}
