29edcc0200e5fff99bc6f5e1d40e5252b703cae2
Your Name <you@example.com>
2025-12-15 07:35:12 +0000

    m

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: pointer to string
+ * @src: pointer to a string
+ * @n: variable to an integer
+ *
+ * Return: 1
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
+
+
+}
