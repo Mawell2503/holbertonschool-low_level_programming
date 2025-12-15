e943b73a95ce07749552ec5e43805b30cdbb5e0d
Your Name <you@example.com>
2025-12-15 07:17:23 +0000

    m

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -2,8 +2,29 @@
 #include "main.h"
 
 /**
- * _strncpy - string copied
- * @dest:
- * @src:
- * @n:
+ * _strncpy - merges 2 strings together
+ * @dest: pointer of the first string
+ * @src:pointer of the second string
+ * @n:a vriable to an integer
  *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int d = 0;
+	int s = 0;
+	int nd;
+
+	if (dest[d] != '\0')
+		d++;
+
+	
+	if (src[s] != '\0')
+		dest[d] = src[s];
+		d++;
+		s++;
+
+	dest[d]  '\0';
+	return (dest);
+}
