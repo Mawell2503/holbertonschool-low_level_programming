b3fe4f797f2350273d5551c35515243d476db944
Your Name <you@example.com>
2025-12-16 04:05:01 +0000

    m

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -12,19 +12,29 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_index = 0;
 	int src_index = 0;
+	int dest_index = 0;
 
-	if (dest[d] != '\0')
-		d++;
+	if (dest == NULL || src == NULL || n < 0)
+		return NULL;
 
-	if (s < n)
-		s++;
+	for (; dest[dest_index] != '\0'; dest_index++)
+		;
 
-	dest[d] = src[s];
-	d++;
-	s++;
+	for (; src[src_index] != '\0'; src_index++)
+		;
 
-	dest[d] = '\0';
-	return (dest);
+	if (n > src_index - 1)
+		return (-1);
+
+	src_index = 0;
+
+	while (src_index < n)
+	{
+		dest[dest_index] = src[src_index];
+		dest_index++;
+		src_index++;
+	}
+
+	return dest;
 }
