f70b5fb85f095a1e367f0c906f605a1303f5da16
Your Name <you@example.com>
2025-12-22 07:16:50 +0000

    m:

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * mul- multiplies two integers
+ * @a: variable of an integer that is the first factor
+ * @b: variable of an integer that is the second factor
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+
+	return (prod);
+}
