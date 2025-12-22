253d982b50b353e79bb41a776f9bc71bdd7db522
Your Name <you@example.com>
2025-12-22 11:01:35 +0000

    m

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -18,9 +18,9 @@ void more_numbers(void)
 		while (n < 15)
 		{
 			if (n > 9)
-				_putchar(n / 10+ '0');
+				_putchar(n / 10 + '0');
 			_putchar(n % 10 + '0');
-			n++;	
+			n++;
 		}
 		_putchar('\n');
 		rows++;
