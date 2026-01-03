aff8ced4e5fb908fecdee2d26b0711a46a65ba3e
Your Name <you@example.com>
2026-01-03 19:11:01 +0000

    m

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	int i, j;
 
 	if (width <= 0 || height <= 0)
-		return (NULL);		//check
+		return (NULL);
 
 	grid = malloc(sizeof(int *) * height);
 	if (grid == NULL)
