5c13f7692f94c721c76ed160ef7c33500d5b5a3d
Your Name <you@example.com>
2026-01-03 19:01:32 +0000

    m

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,6 +10,25 @@
 char *cap_string(char *s)
 {
 	int sep;
+	int is_sep = (sep == ' ' || sep == ',' || sep == ';' || sep == '.' || sep == '!' || sep == '?');
+	int i = 0;
 
-	if (sep = ',' || sep = ';' || sep = '.' || sep = '!' || sep = '?' || seop
+	if (s[i] >= 'a' && s[i] <= 'z')
+	{
+		s[i] = s[i] - 32;
+	}
+	while (s[i] != '\0')
+	{
+		if (s[i] == is_sep)
+		{
+			i++;
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] = s[i] - 32;
+			}
+
+		}
+		i++;
+	}
+	return (s);
 }
