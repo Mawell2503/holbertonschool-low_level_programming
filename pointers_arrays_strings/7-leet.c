a2fca8b017fc608b00ff34e81011627d84927e9c
Your Name <you@example.com>
2026-01-09 09:28:14 +0000

    m

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -7,7 +7,29 @@
  * return:0
  */
 
-char *leet(char *)
+char *leet(char *s)
 {
+	int i;
+	int num_arr[5];
+	int char_arr[5];
 
+	if (s[i] == 'a' || s[i] == 'A') 
+		num_arr[0]=s[i];
+	if (s[i] == 'e' || s[i] == 'E')
+		num_arr[1]=s[i];
+	if (s[i] == 'o' || s[i] == 'O')
+		num_arr[2]=s[i];
+	if (s[i] == 't' || s[i] == 'T')
+		num_arr[3]=s[i];
+	if (s[i] == 'l' || s[i] == 'L')
+		num-arr[4]=s[i];
+
+	char_arr[0]=
+	char_arr[1]=
+	char_arr[2]=
+	char_arr[3]=
+	char_arr[4]=
+
+	for
 }
+
