03389a1ee3ee633d8caa7711b390da7fa61b1d63
Your Name <you@example.com>
2026-01-09 06:40:19 +0000

    m

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - function that draws a straight line
+ * @n: variable of an integer
+ *
+ * Return: 1
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (i = 0; i != n ; i++)
+	{
+		_putchar('_');
+	}
+
+	_putchar('\n');
+}
