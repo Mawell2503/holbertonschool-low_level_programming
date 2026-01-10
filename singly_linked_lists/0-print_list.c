a25c323bb253bd4ab87ebf08ef37e2283a0604af
Your Name <you@example.com>
2026-01-10 15:41:15 +0000

    m

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,17 +9,24 @@
  * Return: count
  */
 
+/*h is a head pointer*/
 size_t print_list(const list_t *h)
 {
+	/*keeps count of nodes being print*/
 	size_t count = 0;
+
+	/*current is a traversal pointer for h*/
 	const list_t *current = h;
 
+	/*loop until we reach NULL(which means no next node)*/
 	while (current != NULL)
 	{
+		/*if theres no string*/
 		if (current -> str ==NULL)
 			printf("[0] (nil)\n");
 
 		else
+			/*prints the length of the string and the string itself*/
 			printf("[%u] %s\n",current -> len, current -> str);
 
 		count++;
