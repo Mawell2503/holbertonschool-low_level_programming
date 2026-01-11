83680453d00434a6696211b4f635a5a725bd600f
Your Name <you@example.com>
2026-01-11 17:50:55 +0000

    m

---
+++ structures_typedef/main.h
@@ -0,0 +1,9 @@
+#ifndef DOG_H
+#define DOG_
+
+Hvoid init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
