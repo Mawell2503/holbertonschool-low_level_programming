0029d27ead020f5e91ec676de15822b148dd2d7e
Your Name <you@example.com>
2026-01-11 16:11:24 +0000

    m

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * init_dog - fills existing struct dog
+ * @d:pointer of where information to be filled is.
+ * @name: points to name
+ * @age: age
+ * @owner: points to owner
+ *
+ * Return: 0
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if( d == NULL)
+		return;
+
+	d -> name = name;
+
+	d -> age = age;
+
+	d -> owner = owner;
+}
