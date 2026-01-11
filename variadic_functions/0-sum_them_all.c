d4895035566e3afb46a2d2a4727ff96d0ba5661d
Your Name <you@example.com>
2026-01-11 19:31:29 +0000

    m

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -10,18 +10,19 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	int sum = 0;
+	va_list args;
+	unsigned int i;
+
 	if (n == 0)
 		return (0);
 
-	int sum = 0;
-
-	va_list args;
 	va_start(args, n);
 
-	for(unsigned int i = 0; i < n; i++)
-		sum = sum + va_args(args, int);
+	for(i = 0; i < n; i++)
+		sum = sum + va_arg(args, int);
 
 	va_end(args);
 
-	return sum:
+	return sum;
 }
