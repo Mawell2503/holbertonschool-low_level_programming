84033edc497b02a8cf005fc34214e96604ace231
Your Name <you@example.com>
2026-09-08 12:22:42 +0400

    m

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -10,29 +10,29 @@
 char *leet(char *str)
 {
 	int i = 0;
-	// 'i' represents the position of a character in str
+	/* 'i' represents the position of a character in str */
 	int j;
-	// 'j' represents the position index of s[1] & s[2]
+	/* 'j' represents the position index of s[1] & s[2] */
 	char s1[] = "aAeEoOtTlL";
-	// s[1] is the letters to be replaced
+	/* s[1] is the letters to be replaced */
 	char s2[] = "4433007711";
-	// s[2] is the numbers we will use to replace the letters in s[1]
+	/* s[2] is the numbers we will use to replace the letters in s[1] */
 
 	while (str[i] != '\0')
-	// while read the string from first index(i=0) to the null terminator
+	/* while read the string from first index(i=0) to the null terminator */
 	{
 		for (j = 0; j < 10; j++)
-		// for every index you go through in str, increment from 0 to 10 for j
+		/* for every index you go through in str, increment from 0 to 10 for j */
 		{
 			if (str[i] == s1[j])
-			// if an index in str correspond to an index in s1
+			/* if an index in str correspond to an index in s1 */
 			{
-				str[i] == s2[j];
-				// replace index in str w the same index position of s2
+				str[i] = s2[j];
+				/* replace index in str w the same index position of s2 */
 			}
 		}
 		i++;
-		// move along the string
+		/* move along the string */
 	}
 	return (str);
 }
